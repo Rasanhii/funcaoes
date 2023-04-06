7ad527db59b2ad94b9e5987838aa905983507b89
Jonatas <jonataspereira788@gmail.com>
2023-04-06 11:16:17 -0300

    Bem legal

--- fun16.py
+++ fun16.py
@@ -1,10 +1,16 @@
 import random
 import string
 
-def gerar_senha(tamanho = 8):
-    caracteres = string.ascii_letters + string.digits + string.punctuation
-    senha="".join(random.choice(caracteres) for i in range(tamanho))
-    return senha
+while True:
+    def gerar_senha(tamanho = 8):
+        caracteres = string.ascii_letters + string.digits + string.punctuation
+        senha="".join(random.choice(caracteres) for i in range(tamanho))
+        return senha
+        
+    senha = gerar_senha()
+    print('A senha é:',senha)
     
-senha = gerar_senha()
-print('A senha é:',senha)
+    pergun = input('Deseja gerar outra senha aleatória? (s/n)')
+    fim = pergun.lower
+    if fim == 'n':
+        break
